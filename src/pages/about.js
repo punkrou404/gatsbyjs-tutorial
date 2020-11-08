ecd9c6d29ad6abfdee2a78a5705745e62c0dc594
punkrou404 <punkrou404@gmail.com>
2020-11-08 11:45:33 +0900

    tutorial: https://www.gatsbyjs.com/tutorial/part-three/ done

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,20 @@
 import React from "react"
+import { Link } from "gatsby"
 import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default function About() {
     return (
-        <div style={{ color: `teal` }}>
+        <Layout>
+            <div>
+                <h1>About me</h1>
+                <p>
+                    I’m good enough, I’m smart enough, and gosh darn it, people like me!
+                </p>
+            </div>
             <Header headerText="About Gatsby" />
             <Header headerText="It's pretty cool" />
             <p>Such wow. Very React.</p>
-        </div>
+        </Layout>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,21 @@
 import { Link } from "gatsby"
 import React from "react"
 import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default function Home() {
     return (
-        <div style={{ color: `purple` }}>
-            <Link to="/contact/">Contact</Link>
+        <Layout>
+            <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+                <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+                <p>
+                    What do I like to do? Lots of course but definitely enjoy building
+                    websites.
+                </p>
+            </div>
             <Header headerText="Hello Gatsby!" />
             <p>What a world.</p>
             <img src="https://source.unsplash.com/random/400x200" alt="" />
-        </div>
+        </Layout>
     )
 }
